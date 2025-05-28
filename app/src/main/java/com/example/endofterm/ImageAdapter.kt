b7518abf666f08d410db9159fcc112f57d6f3107
Yun <wxyun19@gmail.com>
2025-05-28 18:48:01 +0800

    init

---
+++ app/src/main/java/com/example/endofterm/ImageAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.endofterm
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter(private val imageResIds: IntArray) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageResIds[position])
+//        Log.d("ImageAdapter", "Binding image at position $position")
+
+    }
+
+    override fun getItemCount(): Int = imageResIds.size
+}
